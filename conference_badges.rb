574d753be9dfb4d4e92876b2cbc171394b77603f
Max Powell <max_f_powell@hotmail.co.uk>
2019-01-29 17:43:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,6 +13,6 @@ def assign_rooms(array)
 end
 
 def printer(array)
-  puts batch_badge_creator(array)
-  puts assign_rooms(array)
+  batch_badge_creator(array).each {|x| puts x}
+  assign_rooms(array).each {|x| puts x}
 end
