4d28cd0386224f2e9a0d8b04afa3303307600e05
Max Powell <max_f_powell@hotmail.co.uk>
2019-01-29 17:43:19 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  array.collect {|name| badge_maker(name)}
+end
+
+def assign_rooms(array)
+  rooms = []
+  array.each_with_index {|name, index| rooms.push("Hello, #{name}! You'll be assigned to room #{index+1}!")}
+  rooms
+end
+
+def printer(array)
+  batch_badge_creator(array).each {|x| puts x}
+  assign_rooms(array).each {|x| puts x}
+end
