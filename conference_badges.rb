e0e5863bdbf28ff14490f58b3ad3b00dfb76d8e8
Max Powell <max_f_powell@hotmail.co.uk>
2019-01-29 17:34:28 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,3 +5,11 @@ end
 def batch_badge_creator(array)
   array.collect {|name| badge_maker(name)}
 end
+
+def assign_rooms(array)
+  rooms = []
+  array.each_with_index {|name, index|
+    rooms.push("Hello, #{name, }! You'll be assigned to room #{index+1}")
+  }
+  rooms
+end
